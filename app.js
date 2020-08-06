01cc68e644ba0895333eeeed208dacc503573905
Evgen <yujinwork19@gmail.com>
2020-08-06 20:59:48 +0300

    complate burger menu

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-new Glide('.glide').mount()
+new Glide('.glide').mount();
+
+// mobile menu
+
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click",function(){
+	mobileMenu.classList.toggle("active-menu");
+	if(mobileMenu.classList.contains("active-menu")){
+		mainMenu.classList.add("active-menu");
+	}else{
+		mainMenu.classList.remove("active-menu");
+	}
+})
